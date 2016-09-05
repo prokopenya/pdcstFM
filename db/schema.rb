97ea37fe29daeb01e92144726727cc90a992bf87
prokopenya <igorprokopenya@gmail.com>
2016-09-05 15:30:23 +0300

    Add episode model & associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160904145012) do
+ActiveRecord::Schema.define(version: 20160905122241) do
+
+  create_table "episodes", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "podcast_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["podcast_id"], name: "index_episodes_on_podcast_id"
+  end
 
   create_table "podcasts", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
